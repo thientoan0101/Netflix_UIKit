9a7c3c36f091e4927dbf9e7da38fc082c96a7b8b
Nguyen Tran Thien Toan <thientoancubi@gmail.com>
2022-06-27 17:41:02 +0700

    add item cell for upcoming screen

--- NetflixClone/Views/TitleTableViewCell.swift
+++ NetflixClone/Views/TitleTableViewCell.swift
@@ -24,6 +24,8 @@ class TitleTableViewCell: UITableViewCell {
     
     private let titleLabel: UILabel = {
         let label = UILabel()
+//        label.contentMode = .scaleToFill
+//        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -31,6 +33,7 @@ class TitleTableViewCell: UITableViewCell {
     private let titlePosterUIImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
